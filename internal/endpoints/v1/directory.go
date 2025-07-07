b410a714cfebc35ed5f1eabfb5b454f44efe5fe2
ian kopke <iankopke2005@Gmail.com>
2025-07-07 03:24:42 -0400

    CRUD for directory works not sure why there is an error and subsequenly a internalservererror but the db is changing, anyway I need to add some comments and modularize it but it functions

--- internal/endpoints/v1/directory.go
+++ internal/endpoints/v1/directory.go
@@ -27,7 +27,46 @@ func RegisterDirectoryRoutes(router *mux.Router) {
 		w.Write([]byte("Directory endpoint"))
 	}).Methods("GET")
 
-	router.HandleFunc("/directory/{did}", func(w http.ResponseWriter, r *http.Request) {
+	//grabs all the directories
+	router.HandleFunc("/directory/all", func(w http.ResponseWriter, r *http.Request) {
+		rows, err := srv.DB.Query("SELECT name, creator, create_date FROM directory;")
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error selecting from the directory table %v", err)
+			return
+		}
+		defer rows.Close()
+
+		//iterates through all of the rows that returned to put them in an array of maskedDirectory structs
+		var dirs []maskedDirectory
+		for rows.Next() {
+			var dir maskedDirectory
+			if err := rows.Scan(&dir.Name, &dir.Creator, &dir.CreateDate); err != nil {
+				w.WriteHeader(http.StatusInternalServerError)
+				log.Printf("error scanning directory row %v", err)
+				return
+			}
+			dirs = append(dirs, dir)
+		}
+		if err := rows.Err(); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error iterating directory rows %v", err)
+			return
+		}
+
+		j, err := json.Marshal(dirs)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error marshalling directories into json %v", err)
+			return
+		}
+		log.Printf("j = %v\n", j)
+		w.Write(j)
+		w.Header().Set("Content-Type", "application/json")
+	}).Methods("GET")
+
+	//Returns the directory with the directory id did from within the http
+	router.HandleFunc("/directory/{did:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
 		var dir maskedDirectory
 		vars := mux.Vars(r)
 		did := vars["did"]
@@ -44,45 +83,88 @@ func RegisterDirectoryRoutes(router *mux.Router) {
 			log.Printf("error marshalling books into json %v", err)
 			return
 		}
+		log.Printf("j = %v\n", j)
 		w.Write(j)
 
 	}).Methods("GET")
 
+	//Creates directory with data from the body of the HTTP request
 	router.HandleFunc("/directory/create", func(w http.ResponseWriter, r *http.Request) {
 		var body directory
 		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
 			w.WriteHeader(http.StatusBadRequest)
-			log.Printf("error decoding request body into CreateBookBody struct %v", err)
+			log.Printf("error decoding request body into Directory struct %v", err)
 			return
 		}
+		log.Printf("body: %v\n", body)
 
-		sqlRes, err := srv.DB.Exec("INSERT INTO directory (name, creator) VALUES ($1, $2);", body.Did, body.Creator)
-		if err != nil {
+		if err := srv.DB.QueryRow("INSERT INTO directory (name, creator, create_date) VALUES ($1, $2, CURRENT_DATE);", body.Name, body.Creator).Err(); err != nil {
 			w.WriteHeader(http.StatusInternalServerError)
 			log.Printf("error inserting directory into directory table %v", err)
 			return
 		}
-		log.Print(sqlRes)
-		// var dir maskedDirectory
-		// did, err := sqlRes.LastInsertId()
-		// if err != nil {
-		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	log.Printf("error inserting directory into directory table %v", err)
-		// 	return
-		// }
-		// if err := srv.DB.QueryRow("SELECT name, creator, create_date FROM directory WHERE did = $1;", did).Scan(&dir.Name, &dir.Creator, &dir.CreateDate); err != nil {
+
+	}).Methods("POST")
+
+	//Updates the directory with Directory id (did) did from within the HTTP request, updated values is within the body of the request
+	router.HandleFunc("/directory/update/{did:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		var dir maskedDirectory
+		vars := mux.Vars(r)
+		did := vars["did"]
+		log.Printf("vars: %v\n", vars)
+		var body directory
+		if err := json.NewDecoder(r.Body).Decode(&body); err != nil {
+			w.WriteHeader(http.StatusBadRequest)
+			log.Printf("error decoding request body into Directory struct %v", err)
+			return
+		}
+		if err := srv.DB.QueryRow("UPDATE directory SET name = $1, creator = $2, create_date = CURRENT_DATE WHERE did = $3", body.Name, body.Creator, did); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("Error updating the directory %v, %v, %v, %v, with error %v", body.Name, body.Creator, body.CreateDate, did, err)
+			return
+		}
+
+		if err := srv.DB.QueryRow("SELECT name, creator, create_date FROM directory WHERE did = $1;", did).Scan(&dir.Name, &dir.Creator, &dir.CreateDate); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error selecting from the directory table %v", err)
+			return
+		}
+
+		j, err := json.Marshal(dir)
+		if err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error marshalling books into json %v", err)
+			return
+		}
+		log.Printf("j = %v\n", j)
+		w.Write(j)
+
+	}).Methods("PUT")
+
+	//Deletes the directory with Directory id (did) did from within the HTTP request
+	router.HandleFunc("/directory/{did:[0-9]+}", func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		did := vars["did"]
+		log.Print("Delete called")
+		if err := srv.DB.QueryRow("DELETE from directory WHERE did = $1;", did); err != nil {
+			w.WriteHeader(http.StatusInternalServerError)
+			log.Printf("error deleting from the directory table %v", err)
+			return
+		}
+		// temp := 1
+		// if err := srv.DB.QueryRow("SELECT COALESCE((SELECT COUNT(*) from directory WHERE did = $1), 0)", did).Scan(&temp); err != nil {
 		// 	w.WriteHeader(http.StatusInternalServerError)
 		// 	log.Printf("error selecting from the directory table %v", err)
 		// 	return
 		// }
-
-		// j, err := json.Marshal(dir)
-		// if err != nil {
+		// log.Printf("temp = %d", temp)
+		// if temp != 0 {
 		// 	w.WriteHeader(http.StatusInternalServerError)
-		// 	log.Printf("error marshalling books into json %v", err)
+		// 	log.Printf("Error deleting from the directory table, record still exists, %v", temp)
 		// 	return
 		// }
-		// w.Write(j)
+		w.WriteHeader(http.StatusOK)
+		log.Printf("Deleted row with ID %v", did)
+	}).Methods("DELETE")
 
-	}).Methods("POST")
 }
